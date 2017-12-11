3d00f53b4a212da65de0a396209a5070197e5e95
andrey-homenko <homandrej@gmail.com>
2017-12-12 00:23:19 +0200

    Removed SPA

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,12 @@
-/**
- * Global object of App
- */
-var app = {};
-
-/**
- * Base URL
- */
-var BASE_URL = 'templates/';
-
-/**
- * Home page URL
- */
-var homeTemplateUrl = BASE_URL + 'home.html';
-
 $(document).ready(function () {
-
-    var $routerBlock = $('#routerBlock');
-
-    app.renderHome = function () {
-        var displayHome = function (template) {
-            $routerBlock.append(template);
-        };
-
-        $.get(homeTemplateUrl)
-                .then(displayHome)
-                .then(includeModules);
-    };
-
-    app.renderHome();
-
-    app.navigation = function () {
-
-    };
 
     /**
      * Include modules list
      */
-    function includeModules() {
-        //= partials/init.js
-        //= partials/fixed-header.js
-        //= partials/site-guide.js
-        //= partials/go-to-top.js
-        //= partials/project-bg-animation.js
-    }
+    //= partials/init.js
+    //= partials/fixed-header.js
+    //= partials/site-guide.js
+    //= partials/go-to-top.js
+    //= partials/project-bg-animation.js
 
 });
