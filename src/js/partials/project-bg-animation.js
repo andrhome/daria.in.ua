91d423e6bdf3f1e2861a5bcc8e32d5b46b158e01
andrhome <homandrej@gmail.com>
2017-12-14 01:52:27 +0000

    Zernobot images

--- src/js/partials/project-bg-animation.js
+++ src/js/partials/project-bg-animation.js
@@ -1,52 +1,56 @@
 /**
  * Project background animation
  */
-;(function() {
-  'use strict';
+;(function () {
+    'use strict';
 
-  var $projects = $('.project');
-  var $projectPage = $('.project-page');
+    var $projects = $('.project');
+    var $projectPage = $('.project-page');
 
-  if ($projectPage.length > 0) return;
+    if ($projectPage.length > 0) return;
 
-  $projects.on('mouseover', function () {
-    var $this = $(this);
+    function mouseover() {
+        var $this = $(this);
 
-    setTimeout(function () {
-      $this.find('.project__bg').fadeIn(500);
-    }, 100);
+        setTimeout(function () {
+            $this.find('.project__bg').fadeIn(500).stop(true, true);
+        }, 100);
 
-    $(this).find('.project__canvas').css({
-      boxShadow: 'none',
-      background: 'none'
-    });
-  }).on('mouseleave', function () {
-    var $this = $(this);
-
-    function animate(boxShadowColor, bgColor) {
-      $this.find('.project__canvas').css({
-        boxShadow: '0 0 13px 1px ' + boxShadowColor,
-        backgroundColor: bgColor
-      });
+        $(this).find('.project__canvas').css({
+            boxShadow: 'none',
+            background: 'none'
+        });
     }
 
-    setTimeout(function () {
-      $this.find('.project__bg').fadeOut(500);
-    }, 100);
+    function mouseleave() {
+        var $this = $(this);
 
-    setTimeout(function () {
-      if ($this.hasClass('project-zernobot')) {
-        animate('#08171c', '#194957');
-      }
+        function animate(boxShadowColor, bgColor) {
+            $this.find('.project__canvas').css({
+                boxShadow: '0 0 13px 1px ' + boxShadowColor,
+                backgroundColor: bgColor
+            });
+        }
 
-      if ($this.hasClass('project-logbook')) {
-        animate('#f2f2f2', '#f2f2f2');
-      }
+        setTimeout(function () {
+            $this.find('.project__bg').fadeOut(800).stop(true, true);
+        }, 300);
 
-      if ($this.hasClass('project-street-quests')) {
-        animate('#060606', '#2c2c2c');
-      }
-    }, 500)
-  });
+        setTimeout(function () {
+            if ($this.hasClass('project-zernobot')) {
+                animate('#08171c', '#194957');
+            }
+
+            if ($this.hasClass('project-logbook')) {
+                animate('#f2f2f2', '#f2f2f2');
+            }
+
+            if ($this.hasClass('project-street-quests')) {
+                animate('#060606', '#2c2c2c');
+            }
+        }, 300)
+    }
+
+    $projects.hover(mouseover, mouseleave);
 
 }());
