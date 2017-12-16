0a22d41d2736bc908db83c141dfc05f502703fa8
andrhome <homandrej@gmail.com>
2017-12-16 15:00:43 +0200

    Feedback form validation is ready.

--- src/js/partials/form-validation.js
+++ src/js/partials/form-validation.js
@@ -5,30 +5,63 @@
   'use strict';
 
   var $feedbackForm = $('#feedbackForm');
+  var fbFormEmptyIsValid = false;
+  var fbFormEmailIsValid = false;
 
-  $feedbackForm.on('submit', validation.bind($feedbackForm));
-
-  function validation($form, e) {
+  $feedbackForm.on('submit', function (e) {
     e.preventDefault();
-    onRequiredFields($form);
-  }
 
-  function onRequiredFields($form) {
+    checkOnEmptyField($(this));
+    checkEmail($(this));
+
+    if (fbFormEmptyIsValid && fbFormEmailIsValid) {
+      sendForm($(this));
+    }
+  });
+
+  function checkOnEmptyField($form) {
     var $fields = $form.find('.form-field');
 
     $.each($fields, function (i, item) {
-      if (item.val() === '') {
-        item.addClass('required-error');
-        item.next().show();
+      if ($(item).val() === '') {
+        $(item).parent().addClass('required-error');
+        fbFormEmptyIsValid = false;
       }
     });
 
     $fields.on('keypress', function () {
-      console.log('keypress');
-      $(this).removeClass('required-error');
-      $(this).parent().find('.required-message').hide();
+      $(this).parent().removeClass('required-error email-error');
+      fbFormEmptyIsValid = true;
     });
 
+  }
+
+  function checkEmail($form) {
+    var $emailField = $form.find('.form-field-email'),
+      emailVal = $emailField.val();
+
+    if ($emailField.length <= 0) return;
+
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!regex.test(emailVal)) {
+      $emailField.parent().addClass('email-error');
+      fbFormEmailIsValid = false;
+    } else {
+      $emailField.parent().removeClass('email-error');
+      fbFormEmailIsValid = true;
+    }
+  }
+
+  function sendForm($form) {
+    $.ajax({
+      type: 'POST',
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      success: function (response) {
+        console.log(response);
+      }
+    });
   }
 
 }());
