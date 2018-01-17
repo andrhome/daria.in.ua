291cb921f704bc8119c09bdcd330ce1d559dd057
andrhome <homandrej@gmail.com>
2018-01-17 21:55:43 +0200

    About page full responsive +  Zernobot page responsive to 768px.

--- Gulpfile.js
+++ Gulpfile.js
@@ -175,4 +175,4 @@ gulp.task('clean', function (cb) {
   rimraf(path.clean, cb)
 });
 
-gulp.task('default', ['build', /*'webserver',*/ 'watch']);
+gulp.task('default', ['build', 'webserver', 'watch']);
